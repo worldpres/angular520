9e5228511bb081352b9f4cb048f46dbb0cd7a11e
dpres <thepressu@gmail.com>
2018-05-14 13:08:24 +0200

    sortDatabase() & alphabet()

--- src/app/login/show/show.component.ts
+++ src/app/login/show/show.component.ts
@@ -24,6 +24,29 @@ export class ShowComponent implements OnInit {
     {
       "name" : "opony od samochodu osobowego",
       "place" : ["wystawka", "PSZOK"],
+    },
+    {
+      "name" : "toster2",
+      "place" : ["wystawka", "PSZOK"],
     }
   ]
+
+  public sortDatabase(prop, asc=true) {
+    return this.database.sort(function(a, b) {
+      if(asc) return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+      else return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+    });
+  }
+
+  public sortedDatabase = this.sortDatabase('name');
+
+  public alphabet(){
+    let letters = [];
+    for (const obj of this.database) {
+      if(obj.name) letters.push(obj.name.substr(0,1));
+    }
+    letters = letters.filter(function (item, index, array) {return array.indexOf(item) == index;});
+    return letters.sort();
+  }
+
 }
