5fecb90b97a0ec99ff6ffbc31c9be895562d655e
dpres <thepressu@gmail.com>
2018-05-23 15:07:18 +0200

    mongoDB - import from json

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,25 +4,54 @@ const router = express.Router();
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
+});
 
 
-  /**
-   * MongoDB connection
-   */
-  const MongoClient = require('mongodb').MongoClient;
-  const url = "mongodb://whichbin:whichbin@ds233320.mlab.com:33320/whichbin";
+
+/* MongoDB connection */
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://whichbin:whichbin@ds233320.mlab.com:33320/whichbin';
+
+router.get('/insert', (req, res) => {
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    var dbo = db.db("whichbin");
-    var myobj = { name: 'artykuły higieniczne', place: 'odpady zmieszane' };
-    dbo.collection("whichbin").insertOne(myobj, function(err, res) {
+    var dbo = db.db('whichbin');
+    var myobj = { name: 'testowy', place: 'testowe' };
+    dbo.collection('whichbin').insertOne(myobj, function(err, data) {
       if (err) throw err;
-      console.log("1 document inserted");
       db.close();
+      console.log('insert 1 works');
+      res.send('insert 1 works');
     });
   });
-
-
 });
+
+
+
+var fs = require('fs');
+var path = require('path'), filePath = path.join(__dirname, 'base.json');
+
+router.get('/import', (req, res) => {
+
+  fs.readFile(filePath, {encoding: 'utf-8'}, (err, body) => {
+    if (err) throw err;
+    var tab = JSON.parse(body);
+
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db('whichbin');
+      dbo.collection('whichbin').insert(tab, function(err, data){
+        if (err) throw err;
+        db.close();
+        console.log("import all works");
+        res.send('import all works');
+      });
+    });
+
+  });
+  
+});
+
+
 
 module.exports = router;
